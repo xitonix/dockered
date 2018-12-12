ed8e563d7689e2e7fa7407df1b35dc1fe4e2e70a
Alex <alg@campaignmonitor.com>
2018-12-12 19:40:41 +1100

    Use AES instead of hash to make people happy

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main // import "go.xitonix.io/dockered"
 
 import (
-	"crypto/sha512"
-	"encoding/hex"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -29,10 +32,37 @@ func encrypt(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	hs := sha512.New()
-	hs.Write([]byte(data))
-	_, err := w.Write([]byte(hex.EncodeToString(hs.Sum(nil))))
+	encrypted, err := encryptData([]byte(data))
 	if err != nil {
 		http.Error(w, "Failed to encrypt the data", http.StatusInternalServerError)
+		return
 	}
+	_, err = w.Write([]byte(encrypted))
+	if err != nil {
+		http.Error(w, "Failed to write the response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func encryptData(input []byte) (string, error) {
+
+	// ** ATTENTION **
+	// NEVER EVER EVER commit the encryption key into the source control
+	// This is for demonstration purposes ONLY
+	key := []byte("16 bytes AES key")
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(input))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], input)
+
+	return base64.URLEncoding.EncodeToString(cipherText), nil
 }
